8e1a06858f5ba7124309155bd4a6728584630a86
khalish29 <mamanktoti8@gmail.com>
2021-08-21 09:32:55 +0700

    push login page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './*.html',
+    './**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
